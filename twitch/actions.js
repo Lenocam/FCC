e17997aa3a494455015a63ae8acc8456c35be7c0
Reed MacConnell <reed.macconnell@gmail.com>
2017-09-29 16:14:14 -0400

    set up the basic structure to the twitch challenge links. project tells you if what game they're playing if they're online and the bio if they are not

--- twitch/actions.js
+++ twitch/actions.js
@@ -1,28 +1,67 @@
 $(document).ready(function() {
+  var twitchArray = [
+    "ESL_SC2",
+    "syndicate",
+    "cretetion",
+    "freecodecamp",
+    "summit1g",
+    "habathcx",
+    "riotgames",
+    "noobs2ninjas",
+    "esl_csgo"
+  ];
   var baseUrl = "https://wind-bow.gomix.me/twitch-api/";
   var users = "users/";
-  var user = "freecodecamp";
+  var streams = "streams/"
+  var user = "";
+  var streamUrl = baseUrl + streams;
+  var userUrl = baseUrl + users;
+  var userLinkSetUp = "https://go.twitch.tv/";
 
-  var url = baseUrl + users + user;
+  for (var i = 0; i < twitchArray.length; i++) {
+    user = twitchArray[i];
+    streamingUser(user);
+  }
+  function streamingUser(name) {
+    $.ajax({
+      url: streamUrl + name,
+      dataType: "jsonp",
+      type: "GET",
+      success: function(data) {
+        if (data.stream === null) {
+          ifItsNull(name);
+        } else {
+          $("#output").prepend("<div class=well><a href=" + userLinkSetUp+name + "><h2>" + data.stream.channel.display_name + "</h2></a>" + "<p>" + data.stream.game + "</p></div>");
+          console.log(data);
+        }
 
-  console.log(url);
-
-  $.ajax({
-    url: url,
-    dataType: "jsonp",
-    type: "GET",
-    success: function(data) {
-      var userNameLink = baseUrl + users + data.display_name
-      console.log(data);
-      console.log(userNameLink);
-
-      console.log(data.display_name);
-      // console.log(data._links.self);
-      // $("#output").prepend("<div class=well><a href=" + data[links][self] + "><h2>" + data[name] + "</h2></a>" + "<p>" + data[bio] + "</p></div>");
-      $("#output").prepend("<div><a href=" + userNameLink + "><h2>" + data.display_name + "</h2></a><p>" + data.bio + "</p></div>")
-    }
-  });
+      }
+    });
+  }
 
 
+  function ifItsNull(name) {
+    $.ajax({
+      url: userUrl + name,
+      dataType: "jsonp",
+      type: "GET",
+      success: function(data) {
+        console.log(data);
+        $("#output").append("<div><h2>" + data.display_name + "</h2><p>" + data.bio + "</p></div>");
+        //console.log(user);
+
+      }
+
+    });
+
+  }
 
 });
+
+// USAGE:
+// Replace the Twitch API base URL https://api.twitch.tv/kraken with https://wind-bow.gomix.me/twitch-api. Use this endpoint according to the Twitch API documentation.
+//
+// NOTE:
+// This server caches data to lower the request rate. To prevent abuses this server accepts GET requests only, and serves only routes /users/:user, /channels/:channel, and /streams/:stream. These are more than enough to complete the challenge.
+//
+//
