0ba0d269431e1ab50d7749111b730e695e2ceb31
Reed MacConnell <reed.macconnell@gmail.com>
2018-03-21 05:05:50 -0400

    this isn't complete but I'm pretty close, still working on opening and closing parantheses on phone number

---
+++ advancedAlg/telephoneCheck.js
@@ -0,0 +1,67 @@
+function telephoneCheck(str) {
+  var letterCheck = /^1?[-\s]?\(?([2-9]\d{2})\)?[-\s]?([2-9]\d{2})[-\s]?(\d{4})$/;
+  return letterCheck.test(str) ? true : false;
+
+}
+
+console.log(telephoneCheck("555-555-5555")); //should return a boolean.
+console.log(telephoneCheck("1 555-555-5555")); //should return true.
+console.log(telephoneCheck("1 (555) 555-5555")); //should return true.
+console.log(telephoneCheck("5555555555")); //should return true.
+console.log(telephoneCheck("555-555-5555")); //should return true.
+console.log(telephoneCheck("(555)555-5555")); //should return true.
+console.log(telephoneCheck("1(555)555-5555")); //should return true.
+console.log(telephoneCheck("555-5555")); //should return false.
+console.log(telephoneCheck("5555555")); //should return false.
+console.log(telephoneCheck("1 555)555-5555")); //should return false.
+console.log(telephoneCheck("1 555 555 5555")); //should return true.
+console.log(telephoneCheck("1 456 789 4444")); //should return true.
+console.log(telephoneCheck("123**&!!asdf#")); //should return false.
+console.log(telephoneCheck("55555555")); //should return false.
+console.log(telephoneCheck("(6505552368)")); //should return false
+console.log(telephoneCheck("2 (757) 622-7382")); //should return false.
+console.log(telephoneCheck("0 (757) 622-7382")); //should return false.
+console.log(telephoneCheck("-1 (757) 622-7382")); //should return false
+console.log(telephoneCheck("2 757 622-7382")); //should return false.
+console.log(telephoneCheck("10 (757) 622-7382")); //should return false.
+console.log(telephoneCheck("27576227382")); //should return false.
+console.log(telephoneCheck("(275)76227382")); //should return false.
+console.log(telephoneCheck("2(757)6227382")); //should return false.
+console.log(telephoneCheck("2(757)622-7382")); //should return false.
+console.log(telephoneCheck("555)-555-5555")); //should return false.
+console.log(telephoneCheck("(555-555-5555")); //should return false.
+console.log(telephoneCheck("(555)5(55?)-5555")); //should return false.
+
+// 555-555-5555
+// 1 555-555-5555
+// 1 (555) 555-5555
+// 5555555555
+// 555-555-5555
+// (555)555-5555
+// 1(555)555-5555
+// 555-5555
+// 5555555
+// 1 555)555-5555
+// 1 555 555 5555
+// 1 456 789 4444
+// 123**&!!asdf#
+// 55555555
+// (6505552368)
+// 2 (757) 622-7382
+// 0 (757) 622-7382
+// -1 (757) 622-7382
+// 2 757 622-7382
+// 10 (757) 622-7382
+// 27576227382
+// (275)76227382
+// 2(757)6227382
+// 2(757)622-7382
+// 555)-555-5555
+// (555-555-5555
+// (555)5(55?)-5555
+
+//[^A-Za-z]\b[^-]1|\d{3}[-(]|\d{3}\d{4}/
+//[^A-Za-z"]\b[^-]1?|\d{3}(-\( )? |\d{3}(-\) )?\d{4}/
+//[^A-Za-z]\b[^-] 1{0,1}[( ]?\d{3}[-( ]?|\d{3}[-) ]?\d{4}
+// /^(1{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
+/^1?[-\s]?\(?([2-9]\d{2})\)?[-\s]?([2-9]\d{2})[-\s]?(\d{4})$/
